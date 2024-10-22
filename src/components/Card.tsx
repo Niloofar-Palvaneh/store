0255a3fe27f05b343998cf06336e652c1d8c7cd2
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2024-10-22 17:30:56 +0330

    get products from fake api

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect } from 'react'
 
-
-
-function Card() {
+function Card({ product }: any) {
+    console.log(product)
     return (
-        <div>
-            <div className="container mt-4 mx-auto">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                    <div className="m-3 p-4 card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
-                        <div className="">
-                            <h2 className="text-lg mb-2">
-                                Title
-                                <span className="text-sm text-teal-800 font-mono bg-teal-100 inline rounded-full px-2 align-top float-right animate-pulse">+</span></h2>
-                            <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200">Space, the final frontier.
-                                These are the voyages of the
-                                Starship Enterprise. Its five-year mission: to explore strange new worlds.</p>
+        <div className="container mt-4 mx-auto w-full">
+            <div className="grid grid-cols-1">
+                <div className="m-3 p-4 card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+                    <div className="">
+                        <div className='flex items-cnter justify-center w-full'>
+                            <div className={`w-full h-[150px] bg-contain bg-no-repeat`} style={{ backgroundImage: `url(${product.image})` }}></div>
+                            <span className="z-50 text-sm text-teal-800 font-mono bg-teal-100 inline rounded-full px-2 align-top float-right animate-pulse">+</span>
                         </div>
-                        <div className='flex items-center justify-start gap-2'> 
-                            <p>price: </p>
-                            <p className='font-bold'>23000</p>
-                        </div>
+                        {product.title}
+                        <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200 line-clamp-3">
+                            {product.description}
+                        </p>
+                    </div>
+                    <div className='flex items-center justify-start gap-2'>
+                        <p>price: </p>
+                        <p className='font-bold'>{product.price}</p>
                     </div>
                 </div>
             </div>
--- src/components/CardsWrapper.tsx
+++ src/components/CardsWrapper.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 function CardsWrapper() {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(json => setProducts(json))
+    }, [])
+    console.log(products);
+
     return (
         <div>
             <div>
                 <h1>my shop</h1>
                 <p>basket icon</p>
             </div>
-            <div>
-                <Card />
+            <div className='grid grid-cols-3'>
+                {
+                    products && products.map(product => <Card product={product} />)
+                }
             </div>
         </div>
     )
