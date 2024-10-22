1d70b8c22980afff67841f2e8a1062b28cc940d8
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2024-10-22 14:01:36 +0330

    init and add components

---
+++ src/components/CardsWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './Card'
+
+function CardsWrapper() {
+    return (
+        <div>
+            <div>
+                <h1>my shop</h1>
+                <p>basket icon</p>
+            </div>
+            <div>
+                <Card />
+            </div>
+        </div>
+    )
+}
+
+export default CardsWrapper
